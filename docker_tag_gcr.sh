01b731d0443db21527d84ad632ef7f39ce79f011
Max Xu <xuhuan@live.cn>
2019-02-14 11:43:58 +0800

    tiller-v2.12.3

--- docker_tag_gcr.sh
+++ docker_tag_gcr.sh
@@ -14,11 +14,17 @@
 
 username=jsonbruce
 images=("kube-apiserver:v1.13.1" "kube-controller-manager:v1.13.1" "kube-scheduler:v1.13.1" "kube-proxy:v1.13.1" "pause:3.1" "etcd:3.3.10" "coredns:1.2.6" \
-     	"kubernetes-dashboard-amd64:v1.10.1" "heapster-amd64:v1.5.4" "heapster-influxdb-amd64:v1.5.2" "heapster-grafana-amd64:v5.0.4")
+     	"kubernetes-dashboard-amd64:v1.10.1" "heapster-amd64:v1.5.4" "heapster-influxdb-amd64:v1.5.2" "heapster-grafana-amd64:v5.0.4" \
+		"tiller:v2.12.3")
 
 for image in ${images[@]};
 do
     docker pull $username/$image
-    docker tag $username/$image k8s.gcr.io/$image
+	if [[ $image == tiller* ]]
+	then
+		docker tag $username/$image gcr.io/kubernetes-helm/$image
+	else
+		docker tag $username/$image k8s.gcr.io/$image
+	fi
     docker rmi $username/$image
 done
